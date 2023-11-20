819c7125a690af37332523282480fb01c59f4d23
ScienceMind11 <sciencemind@icloud.com>
2023-11-20 18:20:09 -0500

    Recipes n stuff

--- src/main/java/net/mythos/foundry/FoundryMod.java
+++ src/main/java/net/mythos/foundry/FoundryMod.java
@@ -6,6 +6,7 @@ import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.mythos.foundry.foundation.item.ScytheItem;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
@@ -18,12 +19,12 @@ public class FoundryMod implements ModInitializer {
 	public static final String NAME = "Foundry";
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
 
-	public static Item WOODEN_SCYTHE = registerItem("wooden_scythe", new SwordItem(ToolMaterials.WOOD, 5, -2.4F, new QuiltItemSettings()));
-	public static Item STONE_SCYTHE = registerItem("stone_scythe", new SwordItem(ToolMaterials.STONE, 5, -2.4F, new QuiltItemSettings()));
-	public static Item IRON_SCYTHE = registerItem("iron_scythe", new SwordItem(ToolMaterials.IRON, 5, -2.4F, new QuiltItemSettings()));
-	public static Item GOLDEN_SCYTHE = registerItem("golden_scythe", new SwordItem(ToolMaterials.GOLD, 5, -2.4F, new QuiltItemSettings()));
-	public static Item DIAMOND_SCYTHE = registerItem("diamond_scythe", new SwordItem(ToolMaterials.DIAMOND, 5, -2.4F, new QuiltItemSettings()));
-	public static Item NETHERITE_SCYTHE = registerItem("netherite_scythe", new SwordItem(ToolMaterials.NETHERITE, 5, -2.4F, new QuiltItemSettings()));
+	public static Item WOODEN_SCYTHE = registerItem("wooden_scythe", new ScytheItem(ToolMaterials.WOOD, 5, -2.4F));
+	public static Item STONE_SCYTHE = registerItem("stone_scythe", new ScytheItem(ToolMaterials.STONE, 5, -2.4F));
+	public static Item IRON_SCYTHE = registerItem("iron_scythe", new ScytheItem(ToolMaterials.IRON, 5, -2.4F));
+	public static Item GOLDEN_SCYTHE = registerItem("golden_scythe", new ScytheItem(ToolMaterials.GOLD, 5, -2.4F));
+	public static Item DIAMOND_SCYTHE = registerItem("diamond_scythe", new ScytheItem(ToolMaterials.DIAMOND, 5, -2.4F));
+	public static Item NETHERITE_SCYTHE = registerItem("netherite_scythe", new ScytheItem(ToolMaterials.NETHERITE, 5, -2.4F));
 
 	@Override
 	public void onInitialize(ModContainer mod) {
