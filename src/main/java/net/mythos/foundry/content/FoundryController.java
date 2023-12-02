0ea6fd80176ea074a9c149eb46ebe12c2ff69c7a
ScienceMind11 <sciencemind@icloud.com>
2023-12-01 19:00:19 -0500

    Some... stuff?

--- src/main/java/net/mythos/foundry/content/FoundryController.java
+++ src/main/java/net/mythos/foundry/content/FoundryController.java
@@ -2,13 +2,15 @@ package net.mythos.foundry.content;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-public class FoundryController extends Block {
+public class FoundryController extends BlockWithEntity {
 
 	public FoundryController(Settings settings) {
 		super(settings);
@@ -19,6 +21,12 @@ public class FoundryController extends Block {
 
 
 
+	}
+
+	@Nullable
+	@Override
+	public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+		return null;
 	}
 
 }
